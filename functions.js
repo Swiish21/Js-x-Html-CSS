66ce86181e7b46b3ce439338bc5ac0cfdc3d69e0
Swiiish21 <malcolmotieno@gmail.com>
2024-07-08 09:59:53 +0300

    basic javascript functions

---
+++ functions.js
@@ -0,0 +1,16 @@
+function displayName() {
+    name = "John";
+    return name;
+}
+
+console.log(displayName());
+
+
+function math_opps() {
+    var a = 5;
+    var b = 10;
+    var c = a + b;
+    return c;
+}
+
+console.log(math_opps());
